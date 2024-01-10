092ab2ad639bce07366d843f6d0df8801e442a3e
Litezy <liteb237@gmail.com>
2024-01-10 08:16:06 +0100

    first commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,14 @@
+import "./tasks/interact";
+import "./tasks/deploy";
 import "@nomicfoundation/hardhat-toolbox";
 import "@zetachain/toolkit/tasks";
+import "./tasks/stake";
+import "./tasks/claim";
+import "./tasks/unstake";
+import "./tasks/beneficiary";
+import "./tasks/withdraw";
+import "./tasks/unstake";
+import "./tasks/address";
 
 import { getHardhatConfigNetworks } from "@zetachain/networks";
 import { HardhatUserConfig } from "hardhat/config";
